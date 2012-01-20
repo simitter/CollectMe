6d32c4964848eb0da2464beb62a846ab9eb470ca
tschebel <simon.mitterhofer@gmail.com>
2012-01-20 18:02:06 +0100

    added base title files

---
+++ DB/CM_Title.lua
@@ -0,0 +1,14 @@
+-- general mounts
+CollectMeCommonTitleTable = {
+    176, -- <Name> of the Ashen Verdict
+    189  -- Assistant Professor <Name>
+};
+
+-- alliance mounts
+CollectMeAllianceTitleTable = {
+};
+
+-- horde mounts
+CollectMeHordeTitleTable = {
+
+};
---
+++ DB/CM_TitleInfo_en.lua
@@ -0,0 +1,4 @@
+CollectMeCompanionInfo = {
+    [176] = "Blablabla", -- <Name> of the Ashen Verdict
+    [189] = "BliBliBli", -- Assistant Professor <Name>
+}
