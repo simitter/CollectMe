a41a286d6d50d7f15f8c4f9a06ca56f886061e08
Tschebel <simon.mitterhofer@gmail.com>
2012-07-21 18:46:54 +0200

    base layout and slash commands

--- CollectMe.lua
+++ CollectMe.lua
@@ -1,15 +1,24 @@
-local CollectMe = LibStub("AceAddon-3.0"):NewAddon("CollectMe")
+CollectMe = LibStub("AceAddon-3.0"):NewAddon("CollectMe", "AceConsole-3.0");
+local AceGUI = LibStub("AceGUI-3.0");
 
 function CollectMe:OnInitialize()
     self.COLLECTME_VERSION = GetAddOnMetadata("CollectMe", "Version");
 
-end
+    self:BuildUI();
 
-function CollectMe:OnEnable()
-
-end
-
-function CollectMe:OnDisable()
+    self:RegisterChatCommand("collectme", "SlashProcessor");
+    self:RegisterChatCommand("cm", "SlashProcessor");
 
 end
 
+function CollectMe:BuildUI()
+    local f = AceGUI:Create("Frame");
+    f:SetTitle("Collect Me " .. self.COLLECTME_VERSION);
+    f:Hide();
+
+    self.frame = f;
+end
+
+function CollectMe:SlashProcessor(input)
+    self.frame:Show();
+end
