ab3320586f387245ae7932ee7565f0925dff0b9f
Tschebel <simon.mitterhofer@gmail.com>
2012-07-28 17:17:28 +0200

    UI Scroll Lists

--- CollectMe.lua
+++ CollectMe.lua
@@ -18,20 +18,50 @@ end
 function CollectMe:BuildUI()
     local f = AceGUI:Create("CollectMe");
     f:SetTitle("Collect Me " .. self.COLLECTME_VERSION);
-    f:SetWidth(500);
+    f:SetWidth(570)
     f:SetLayout("Fill")
 
-    local tabs = AceGUI:Create("TabGroup");
+    local tabs = AceGUI:Create("TabGroup")
     tabs:SetTabs({ {text = "Mounts", value = 1}, {text = "Titles", value = 62}})
     tabs:SetCallback("OnGroupSelected", function (container, event, group) CollectMe:SelectGroup(container, group) end)
-    f:AddChild(tabs);
+    f:AddChild(tabs)
 
     self.tabs = tabs
     self.frame = f
 end
 
 function CollectMe:BuildTab(container, group)
-    
+    container:SetLayout("Flow")
+
+    local scrollcontainer = AceGUI:Create("SimpleGroup")
+    scrollcontainer:SetRelativeWidth(0.6)
+    scrollcontainer:SetFullHeight(true)
+    scrollcontainer:SetLayout("Fill")
+    container:AddChild(scrollcontainer)
+
+    local filtercontainer = AceGUI:Create("SimpleGroup")
+    filtercontainer:SetRelativeWidth(0.4)
+    filtercontainer:SetFullHeight(true)
+    filtercontainer:SetLayout("Fill")
+    container:AddChild(filtercontainer)
+
+    local scroll = AceGUI:Create("ScrollFrame")
+    scroll:SetFullHeight(true)
+    scrollcontainer:AddChild(scroll)
+
+    local active = AceGUI:Create("Heading")
+    active:SetText("Active")
+    active:SetFullWidth(true)
+    scroll:AddChild(active)
+
+    local filter = AceGUI:Create("ScrollFrame")
+    filter:SetFullHeight(true)
+    filtercontainer:AddChild(filter)
+
+    local desc = AceGUI:Create("Heading")
+    desc:SetText("Filters")
+    desc:SetFullWidth(true)
+    filter:AddChild(desc)
 end
 
 function CollectMe:SlashProcessor(input)
