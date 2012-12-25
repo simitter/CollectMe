f79ab9c89542001e00b4b90018cd42396b3242e2
Tschebel <simon.mitterhofer@gmail.com>
2012-12-25 18:43:11 +0100

    ldb tooltip

--- src/LdbDisplay.lua
+++ src/LdbDisplay.lua
@@ -2,14 +2,37 @@ CollectMe.LdbDisplay = CollectMe:NewModule("LdbDisplay", "AceEvent-3.0")
 
 function CollectMe.LdbDisplay:OnInitialize()
     self.L = CollectMe.L
+    self.loaded = false
 
     self.dataObject = LibStub("LibDataBroker-1.1"):NewDataObject(CollectMe.ADDON_NAME, {
         type = "data source",
         icon = "Interface\\Icons\\INV_PET_BABYBLIZZARDBEAR",
         text = CollectMe.L["Collectables in this zone"],
 
-        OnClick = function( dataObject )
+        OnClick = function()
+            GameTooltip:FadeOut()
             CollectMe.UI:Show(CollectMe.COMPANION)
+        end,
+
+        OnEnter = function(this)
+            if self.loaded == false then
+                self:ZoneChangeListener()
+            end
+            GameTooltip:SetOwner( this, "ANCHOR_NONE" )
+            GameTooltip:ClearAllPoints()
+            local cx, cy = this:GetCenter()
+            if cy < GetScreenHeight() / 2 then
+                GameTooltip:SetPoint( "BOTTOM", this, "TOP", dx, dy )
+            else
+                GameTooltip:SetPoint( "TOP", this, "BOTTOM", dx, dy )
+            end
+
+            self:UpdateTooltip()
+            GameTooltip:Show()
+        end,
+
+        OnLeave = function()
+            GameTooltip:FadeOut()
         end
     })
 
@@ -26,6 +49,7 @@ function CollectMe.LdbDisplay:UpdateData()
     self.unique_collected_count = 0
     self.missing_count = #self.missing
     self.quality_counts = { [1]=0, [2]=0, [3]=0, [4]=0 }
+    self.zone_name = GetMapNameByID(CollectMe.ZoneDB:Current())
 
     local collected = {}
 
@@ -36,6 +60,7 @@ function CollectMe.LdbDisplay:UpdateData()
             self.unique_collected_count = self.unique_collected_count + 1
         end
     end
+    self.loaded = true
 end
 
 function CollectMe.LdbDisplay:UpdateText()
@@ -63,3 +88,26 @@ function CollectMe.LdbDisplay:UpdateText()
 
     GameTooltip:Hide()
 end
+
+function CollectMe.LdbDisplay:UpdateTooltip()
+    GameTooltip:SetText(self.L["Companions in"] .. " " .. self.zone_name)
+    GameTooltip:AddLine(" ")
+    if self.unique_collected_count > 0 then
+        GameTooltip:AddLine(GREEN_FONT_COLOR_CODE .. self.unique_collected_count .. " " .. self.L["collected"] .. ":" .. FONT_COLOR_CODE_CLOSE)
+        for i,v in ipairs(self.collected) do
+            local name = v.name
+            if(v.custom_name ~= nil) then
+                name = name .. " (".. v.custom_name..")"
+            end
+            GameTooltip:AddDoubleLine(name, CollectMe:ColorizeByQuality("Level " .. v.level, v.color))
+        end
+        GameTooltip:AddLine(" ")
+    end
+    if self.missing_count > 0 then
+        GameTooltip:AddLine(RED_FONT_COLOR_CODE .. self.missing_count .. " " .. self.L["missing"] .. ":" .. FONT_COLOR_CODE_CLOSE)
+        for i,v in ipairs(self.missing) do
+            GameTooltip:AddLine(v.name)
+        end
+    end
+end
+
