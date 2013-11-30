67599ef634bbe909844f8ca09c387d3086005b41
Tschebel <simon.mitterhofer@gmail.com>
2013-11-30 09:40:47 +0100

    switch PetBattle.lua to scoping instead of OO

--- src/PetBattle.lua
+++ src/PetBattle.lua
@@ -1,21 +1,15 @@
 local CollectMe = LibStub("AceAddon-3.0"):GetAddon("CollectMe")
+local PetBattle = CollectMe:NewModule("PetBattle", "AceEvent-3.0")
 
-CollectMe.PetBattle = CollectMe:NewModule("PetBattle", "AceEvent-3.0")
+local enemies = {}
 
-function CollectMe.PetBattle:OnInitialize()
-    self:RegisterEvent("PET_BATTLE_OPENING_START", "ResetEnemyTable")
-    self:RegisterEvent("PET_BATTLE_PET_CHANGED", "CheckEnemyQuality")
-    
-    self.enemies = {}
+local function ResetEnemyTable()
+    enemies = {}
 end
 
-function CollectMe.PetBattle:ResetEnemyTable()
-    self.enemies = {}
-end
-
-function CollectMe.PetBattle:IsInEnemyTable(id, quality)
-    for i = 1, #self.enemies do
-        if self.enemies[i].enemy_species_id == id and self.enemies[i].enemy_quality == quality then
+local function IsInEnemyTable(id, quality)
+    for i = 1, #enemies do
+        if enemies[i].enemy_species_id == id and enemies[i].enemy_quality == quality then
             return i
         end
     end
@@ -23,7 +17,7 @@ function CollectMe.PetBattle:IsInEnemyTable(id, quality)
     return false
 end
 
-function CollectMe.PetBattle:CheckEnemyQuality()
+local function CheckEnemyQuality()
     if CollectMe.db.profile.tooltip.companions.quality_check == true then
         local trapable, trap_error = C_PetBattles.IsTrapAvailable()
         if trap_error == 6 or trap_error == 7 then
@@ -34,15 +28,15 @@ function CollectMe.PetBattle:CheckEnemyQuality()
             local enemy_quality = C_PetBattles.GetBreedQuality(LE_BATTLE_PET_ENEMY,i)
             local quality = -1
 
-            local index = self:IsInEnemyTable(enemy_species_id, enemy_quality)
+            local index = IsInEnemyTable(enemy_species_id, enemy_quality)
             if index == false then
-                tinsert(self.enemies, {
+                tinsert(enemies, {
                     enemy_species_id = enemy_species_id,
                     enemy_quality = enemy_quality,
                     already_printed = false
                 })
 
-                index = self:IsInEnemyTable(enemy_species_id, enemy_quality)
+                index = IsInEnemyTable(enemy_species_id, enemy_quality)
             end
 
             for j,v in ipairs(CollectMe.CompanionDB:Get()) do
@@ -53,15 +47,20 @@ function CollectMe.PetBattle:CheckEnemyQuality()
                 end
             end
 
-            if self.enemies[index].already_printed == false then
+            if enemies[index].already_printed == false then
                 if quality == -1 then
                     CollectMe:Print(C_PetBattles.GetName(2,i).." - " .. CollectMe:ColorizeByQuality(_G["BATTLE_PET_BREED_QUALITY" .. enemy_quality], enemy_quality - 1) .. " - " .. RED_FONT_COLOR_CODE .. CollectMe.L["Missing companion"] .. FONT_COLOR_CODE_CLOSE)
                 elseif quality < enemy_quality then
                     CollectMe:Print(C_PetBattles.GetName(2,i).." - " .. CollectMe:ColorizeByQuality(_G["BATTLE_PET_BREED_QUALITY" .. enemy_quality], enemy_quality - 1) .. " - " .. RED_FONT_COLOR_CODE .. CollectMe.L["This companion has a higher quality than yours"] .. FONT_COLOR_CODE_CLOSE)
                 end
 
-                self.enemies[index].already_printed = true
+                enemies[index].already_printed = true
             end
         end
     end
+end
+
+function PetBattle:OnInitialize()
+    self:RegisterEvent("PET_BATTLE_OPENING_START", ResetEnemyTable)
+    self:RegisterEvent("PET_BATTLE_PET_CHANGED", CheckEnemyQuality)
 end
